4a5482983c7a4f52b88825184ea82409fff8dfec
Medvedevpd <medvedevpd@gmail.com>
2021-09-08 20:14:52 +0300

    Задание 1 - переделала в autoLayout все лейблы, вьюхи и кнопки

--- Navigation/Profile/ProfileHeaderVIew.swift
+++ Navigation/Profile/ProfileHeaderVIew.swift
@@ -14,15 +14,15 @@ class ProfileHeaderVIew: UIView {
 //    let avatarView = UIView()
     let avatarImageView = UIImageView(image: UIImage(named: "Cat"))
     
-    let avatarLayer = CALayer()
-    let avatarImage = UIImage(named: "Cat")?.cgImage
-    
+//    let avatarLayer = CALayer()
+//    let avatarImage = UIImage(named: "Cat")?.cgImage
+//
     //создаем верхний лейбл и нижнюю подпись
 //    var upperLabel = UILabel()
     var fullNameLabel = UILabel()
 //
 //    var lowerLabel = UITextField()
-    var statusLabel = UILabel()
+//    var statusLabel = UILabel()
     
     var statusTextField = UITextField()
     var setStatusButton = UIButton.init(frame: CGRect(x: 16, y: (91 + 100 + 32), width: 360, height: 50))
@@ -44,29 +44,55 @@ class ProfileHeaderVIew: UIView {
         
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
         fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
         statusTextField.translatesAutoresizingMaskIntoConstraints = false
         setStatusButton.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(avatarImageView)
         addSubview(fullNameLabel)
-        addSubview(statusLabel)
         addSubview(statusTextField)
         addSubview(setStatusButton)
         
         avatarImageView.layer.borderColor = UIColor.white.cgColor
         avatarImageView.layer.borderWidth = 3
         avatarImageView.layer.cornerRadius = 50
+      
+        fullNameLabel.text = "Mr Cat"
+        fullNameLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        fullNameLabel.textColor = .black
         
+        statusTextField.placeholder = "Waiting for something..."
+        statusTextField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusTextField.textColor = .gray
         
+        setStatusButton.setTitle("Show status", for: .normal)
+        setStatusButton.backgroundColor = UIColor.systemBlue
+        setStatusButton.layer.shadowOffset = .init(width: 4, height: 4)
+        setStatusButton.layer.shadowRadius = 4
+        setStatusButton.layer.shadowColor = UIColor.black.cgColor
+        setStatusButton.layer.shadowOpacity = 0.7
+        setStatusButton.layer.cornerRadius = 4
+       
+        setStatusButton.addTarget(self, action: #selector(buttonPressed), for: UIControl.Event.touchUpInside)
         
         
         let constraints = [
             avatarImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 16),
             avatarImageView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
             avatarImageView.heightAnchor.constraint(equalToConstant: 100),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 100)
+            avatarImageView.widthAnchor.constraint(equalToConstant: 100),
             
+            fullNameLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 27),
+            fullNameLabel.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -16),
+            fullNameLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 50),
+            fullNameLabel.heightAnchor.constraint(equalToConstant: 30),
+            
+            statusTextField.centerXAnchor.constraint(equalTo: fullNameLabel.centerXAnchor),
+            statusTextField.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 20),
+            statusTextField.leftAnchor.constraint(equalTo: fullNameLabel.leftAnchor),
+            
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
+            setStatusButton.leftAnchor.constraint(equalTo: avatarImageView.leftAnchor),
+            setStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16)
             
         ]
         
@@ -129,8 +155,8 @@ class ProfileHeaderVIew: UIView {
     }
     
     //Указываем функцию работы кнопки и ее отображения
-//    @objc func buttonPressed(sender: UIButton) {
-//        print(lowerLabel.text ?? "Enter something to see in console")
-//    }
+    @objc func buttonPressed(sender: UIButton) {
+        print(statusTextField.text ?? "Enter something to see in console")
+    }
     
 }
