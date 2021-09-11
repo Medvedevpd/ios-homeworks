e4cf8b8f82daa8f5582a9746f706c79592f5b9d0
Medvedevpd <medvedevpd@gmail.com>
2021-09-11 21:41:50 +0300

    исправнее замечания

--- Navigation/Profile/ProfileHeaderVIew.swift
+++ Navigation/Profile/ProfileHeaderVIew.swift
@@ -10,10 +10,59 @@ import UIKit
 
 class ProfileHeaderVIew: UIView {
 
-    let avatarImageView = UIImageView(image: UIImage(named: "Cat"))
-    var fullNameLabel = UILabel()
-    var statusTextField = UITextField()
-    var setStatusButton = UIButton()
+    let avatarImageView: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "Cat"))
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.borderWidth = 3
+        image.layer.cornerRadius = 50
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+        
+    var fullNameLabel: UILabel = {
+    let label = UILabel()
+        label.text = "Mr Cat"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var statusLabel: UILabel = {
+    let label = UILabel()
+        label.text = "Waiting for something..."
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var statusTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "  Waiting for something..."
+        textField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        textField.textColor = .gray
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 12
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var setStatusButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Show status", for: .normal)
+        button.backgroundColor = UIColor.systemBlue
+        button.layer.shadowOffset = .init(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.cornerRadius = 4
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonPressed), for: UIControl.Event.touchUpInside)
+        return button
+    }()
   
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,57 +76,32 @@ class ProfileHeaderVIew: UIView {
     
     func createSubviews() {
         
-        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusTextField.translatesAutoresizingMaskIntoConstraints = false
-        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
-        
         addSubview(avatarImageView)
         addSubview(fullNameLabel)
         addSubview(statusTextField)
         addSubview(setStatusButton)
-        
-        avatarImageView.layer.borderColor = UIColor.white.cgColor
-        avatarImageView.layer.borderWidth = 3
-        avatarImageView.layer.cornerRadius = 50
-      
-        fullNameLabel.text = "Mr Cat"
-        fullNameLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        fullNameLabel.textColor = .black
-        
-        statusTextField.placeholder = "Waiting for something..."
-        statusTextField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        statusTextField.textColor = .gray
-        statusTextField.backgroundColor = .white
-        
-        setStatusButton.setTitle("Show status", for: .normal)
-        setStatusButton.backgroundColor = UIColor.systemBlue
-        setStatusButton.layer.shadowOffset = .init(width: 4, height: 4)
-        setStatusButton.layer.shadowRadius = 4
-        setStatusButton.layer.shadowColor = UIColor.black.cgColor
-        setStatusButton.layer.shadowOpacity = 0.7
-        setStatusButton.layer.cornerRadius = 4
+        addSubview(statusLabel)
        
-        setStatusButton.addTarget(self, action: #selector(buttonPressed), for: UIControl.Event.touchUpInside)
-        
         let constraints = [
             avatarImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 16),
-            avatarImageView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             avatarImageView.heightAnchor.constraint(equalToConstant: 100),
             avatarImageView.widthAnchor.constraint(equalToConstant: 100),
             
-            fullNameLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 27),
-            fullNameLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 50),
-            fullNameLabel.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -16),
-            fullNameLabel.heightAnchor.constraint(equalToConstant: 30),
-            
-            statusTextField.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 20),
-            statusTextField.leftAnchor.constraint(equalTo: fullNameLabel.leftAnchor),
-            statusTextField.centerXAnchor.constraint(equalTo: fullNameLabel.centerXAnchor),
-            
             setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
-            setStatusButton.leftAnchor.constraint(equalTo: avatarImageView.leftAnchor),
+            setStatusButton.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
             setStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+
+            fullNameLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            fullNameLabel.leadingAnchor.constraint(equalTo: self.centerXAnchor),
+            fullNameLabel.heightAnchor.constraint(equalToConstant: 30),
+
+            statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -5),
+            statusLabel.leadingAnchor.constraint(equalTo: statusTextField.leadingAnchor),
+
+            statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -20),
+            statusTextField.leadingAnchor.constraint(equalTo: setStatusButton.centerXAnchor ),
+            statusTextField.heightAnchor.constraint(equalToConstant: 40),
         ]
         
         setNeedsLayout()
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -15,7 +15,19 @@ class ProfileViewController: UIViewController {
             view = shareView
         }
     
-   var newLowerButton = UIButton()
+    var newLowerButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("New button", for: .normal)
+        button.backgroundColor = UIColor.systemPink
+        button.layer.shadowOffset = .init(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(buttonPress), for: UIControl.Event.touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
       
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,25 +44,15 @@ class ProfileViewController: UIViewController {
 
         //добавляем autoResizingMask into Constraints
         profileHV.translatesAutoresizingMaskIntoConstraints = false
-        newLowerButton.translatesAutoresizingMaskIntoConstraints = false
-
-        newLowerButton.setTitle("New button", for: .normal)
-        newLowerButton.backgroundColor = UIColor.systemPink
-        newLowerButton.layer.shadowOffset = .init(width: 4, height: 4)
-        newLowerButton.layer.shadowRadius = 4
-        newLowerButton.layer.shadowColor = UIColor.black.cgColor
-        newLowerButton.layer.shadowOpacity = 0.7
-        newLowerButton.layer.cornerRadius = 4
-        newLowerButton.addTarget(self, action: #selector(buttonPress), for: UIControl.Event.touchUpInside)
         
         let constraints = [
             profileHV.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            profileHV.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 0),
-            profileHV.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            profileHV.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            profileHV.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
             profileHV.heightAnchor.constraint(equalToConstant: 220),
             
-            newLowerButton.leftAnchor.constraint(equalTo: profileHV.leftAnchor),
-            newLowerButton.rightAnchor.constraint(equalTo: profileHV.rightAnchor),
+            newLowerButton.leadingAnchor.constraint(equalTo: profileHV.leadingAnchor),
+            newLowerButton.trailingAnchor.constraint(equalTo: profileHV.trailingAnchor),
             newLowerButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
             newLowerButton.heightAnchor.constraint(equalToConstant: 50)
         ]
